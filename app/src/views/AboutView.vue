7aad1642759d08ba1a02fc207da96a154c6fb85d
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-03-10 12:32:15 -0400

    m

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,3 +1,40 @@
-<template></template>
+<template>
+  <Doughnut id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
 
-<style></style>
+<script>
+import { Doughnut } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, CategoryScale, ArcElement)
+
+export default {
+  name: 'DoughChart',
+  components: { Doughnut },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March'],
+        datasets: [
+          {
+            data: [40, 20, 12],
+            backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    }
+  },
+}
+</script>
