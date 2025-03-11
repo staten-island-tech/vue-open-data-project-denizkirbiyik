907275f7afb1369f93d64ae0e47b205caf3bed5a
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-03-11 12:34:56 -0400

    m

---
+++ app/src/components/DoughnutChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <Doughnut id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
+
+<script setup>
+import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, CategoryScale, ArcElement } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, CategoryScale, ArcElement)
+const props = defineProps({
+  data: Array,
+})
+const chartData = {
+  labels: ['Brooklyn', 'Manhattan', 'Staten Island', 'Bronx', 'Queens'],
+  datasets: [
+    {
+      data: props.data,
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+    },
+  ],
+}
+const chartOptions = {
+  responsive: true,
+}
+</script>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,40 +1,7 @@
 <template>
-  <Doughnut id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <DoughnutChart :data="[43, 453, 23, 54, 2]" />
 </template>
 
-<script>
-import { Doughnut } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  ArcElement,
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, CategoryScale, ArcElement)
-
-export default {
-  name: 'DoughChart',
-  components: { Doughnut },
-  data() {
-    return {
-      chartData: {
-        labels: ['January', 'February', 'March'],
-        datasets: [
-          {
-            data: [40, 20, 12],
-            backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-      },
-    }
-  },
-}
+<script setup>
+import DoughnutChart from '@/components/DoughnutChart.vue'
 </script>
